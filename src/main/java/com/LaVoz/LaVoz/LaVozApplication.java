5cefb72c65e3a913037fe57aceea599dd3c7a3ba
yunseo0219 <chldbstj021902@gmail.com>
2025-06-21 21:42:22 +0900

    🔧chore: 프로젝트 생성

---
+++ src/main/java/com/LaVoz/LaVoz/LaVozApplication.java
@@ -0,0 +1,13 @@
+package com.LaVoz.LaVoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaVozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaVozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LaVoz/LaVoz/LaVozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaVoz.LaVoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaVozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
