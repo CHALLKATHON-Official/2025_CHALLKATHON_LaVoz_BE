f45a408460d83d2e3407d140b7032015f17cf97a
sehyun <sese2204@naver.com>
2025-06-22 15:57:57 +0900

    🔧chore: gitignore 추가

---
+++ src/main/java/com/LaVoz/LaVoz/domain/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.LaVoz.LaVoz.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
