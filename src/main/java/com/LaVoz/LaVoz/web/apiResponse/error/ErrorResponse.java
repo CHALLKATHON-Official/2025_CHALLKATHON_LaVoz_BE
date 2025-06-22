57310013b821111781ada84f85df4875ea439944
yunseo0219 <chldbstj021902@gmail.com>
2025-06-22 15:05:47 +0900

    🔧chore: 오류 응답 헨들러

---
+++ src/main/java/com/LaVoz/LaVoz/web/apiResponse/error/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.LaVoz.LaVoz.web.apiResponse.error;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import lombok.Setter;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class ErrorResponse {
+
+    private Boolean isSuccess;
+    private String code;
+    private String message;
+
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> validation;
+
+}
