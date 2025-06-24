9904a22c69d2227a6bff5c79eaf061c73f173c41
sehyun <sese2204@naver.com>
2025-06-24 12:20:42 +0900

    ✨feat: Note 관련 api 구현
    - 자신이 속한 조직에만 새로운 노트 작성 가능
    - 자신이 속한 조직의 모든 노트 조회
    - 특정 날짜 이후의 모든 노트 조회
    - 노트 삭제

--- src/main/java/com/LaVoz/LaVoz/web/apiResponse/success/SuccessStatus.java
+++ src/main/java/com/LaVoz/LaVoz/web/apiResponse/success/SuccessStatus.java
@@ -28,6 +28,11 @@ public enum SuccessStatus {
     GET_ORGANIZATION_LIST_SUCCESS(HttpStatus.OK,StatusCode.ORGANIZATION.getCode(2001), "Organization이 성공적으로 반환되었습니다."),
     ORGANIZATION_DELETED_SUCCESS(HttpStatus.OK,StatusCode.ORGANIZATION.getCode(2012), "Organization이 성공적으로 삭제되었습니다."),
     MEMBER_ADDED_TO_ORGANIZATION_SUCCESS(HttpStatus.OK,StatusCode.ORGANIZATION.getCode(2012), "Organization에 Member가 성공적으로 추가되었습니다."),
+
+    NOTE_CREATED_SUCCESS(HttpStatus.OK, StatusCode.NOTE.getCode(2011), "Note가 성공적으로 생성되었습니다."),
+    GET_ORGANIZATION_NOTES_SUCCESS(HttpStatus.OK,StatusCode.NOTE.getCode(2001), "Note가 성공적으로 반환되었습니다."),
+    GET_NOTE_DETAIL_SUCCESS(HttpStatus.OK,StatusCode.NOTE.getCode(2002), "Note의 상세정보가 성공적으로 반환되었습니다."),
+    NOTE_DELETED_SUCCESS(HttpStatus.OK,StatusCode.NOTE.getCode(2012), "Note가 성공적으로 삭제되었습니다.")
     ;
 
 
